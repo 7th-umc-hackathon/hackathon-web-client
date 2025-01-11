a260b05132f2145007944bdcdbb80317b2232439
김지나 <jina4066@naver.com>
2025-01-12 01:34:59 +0900

    Fix: app.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LadingPage";
-import PreMain from "./pages/PreRelayMainPage";
-import PostMain from "./pages/PostRelayMainPage";
-import Footer from "./components/Layout/footer";
-import SignupPage from "./pages/SignupPage";
-import MyProfilePage from "./pages/MyProfilePage";
-import JoinRelay from "./pages/JoinRelay";
-import RunningRelay from "./pages/RunningRelay";
-
-import CapturePage from "./pages/CapturePage";
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import PreMain from './pages/PreRelayMainPage';
+import PostMain from './pages/PostRelayMainPage';
+// import Footer from './components/Layout/footer';
+import SignupPage from './pages/SignupPage';
+import MyProfilePage from './pages/MyProfilePage';
+import CapturePage from './pages/CapturePage';
+import JoinRelay from './pages/JoinRelay';
+import RunningRelay from './pages/RunningRelay';
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/premain" element={<PreMain />} />
-        <Route path="/postmain" element={<PostMain />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/myprofile" element={<MyProfilePage />} />
-        <Route path="/capture" element={<CapturePage />} />
-        <Route path="/joinRelay" element={<JoinRelay />} />
-        <Route path="/runningRelay" element={<RunningRelay />} />
-      </Routes>
-      {/* <Footer /> */}
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/premain" element={<PreMain />} />
+                <Route path="/postmain" element={<PostMain />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/myprofile" element={<MyProfilePage />} />
+                <Route path="/capture" element={<CapturePage />} />
+                <Route path="/joinrelay" element={<JoinRelay />} />
+                <Route path="/runningrelay" element={<RunningRelay />} />
+            </Routes>
+            {/* <Footer /> */}
+        </div>
+    );
 }
 
 export default App;
