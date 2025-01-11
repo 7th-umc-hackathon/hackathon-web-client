52ca9c96925dc286219f5896d53eece999ab0806
김지나 <jina4066@naver.com>
2025-01-12 01:24:11 +0900

    Feat: Relay 개설 화면 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LadingPage';
-import PreMain from './pages/PreRelayMainPage';
-import PostMain from './pages/PostRelayMainPage';
-import Footer from './components/Layout/footer';
-import SignupPage from './pages/SignupPage';
-import MyProfilePage from './pages/MyProfilePage';
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LadingPage";
+import PreMain from "./pages/PreRelayMainPage";
+import PostMain from "./pages/PostRelayMainPage";
+import Footer from "./components/Layout/footer";
+import SignupPage from "./pages/SignupPage";
+import MyProfilePage from "./pages/MyProfilePage";
+import JoinRelay from "./pages/JoinRelay";
+import RunningRelay from "./pages/RunningRelay";
 
-import CapturePage from './pages/CapturePage';
+import CapturePage from "./pages/CapturePage";
 function App() {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/premain" element={<PreMain />} />
-                <Route path="/postmain" element={<PostMain />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/myprofile" element={<MyProfilePage />} />
-                <Route path="/capture" element={<CapturePage />} />
-            </Routes>
-            {/* <Footer /> */}
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/premain" element={<PreMain />} />
+        <Route path="/postmain" element={<PostMain />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/myprofile" element={<MyProfilePage />} />
+        <Route path="/capture" element={<CapturePage />} />
+        <Route path="/joinRelay" element={<JoinRelay />} />
+        <Route path="/runningRelay" element={<RunningRelay />} />
+      </Routes>
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
